8672d6915b65d97a9687f394ffb42ef7b8974b06
sam muiga <sammuiga.m@gmail.com>
2021-09-11 21:23:23 +0300

    created login layout

--- app/src/main/java/com/sammuiga/emergram/activities/LoginActivity.kt
+++ app/src/main/java/com/sammuiga/emergram/activities/LoginActivity.kt
@@ -1,12 +1,25 @@
 package com.sammuiga.emergram.activities
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 import com.sammuiga.emergram.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
     }
 }
