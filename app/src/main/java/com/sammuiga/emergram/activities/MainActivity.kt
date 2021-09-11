509088a9429aa6016a3637a0981371efda9b640c
sam muiga <sammuiga.m@gmail.com>
2021-09-12 02:47:22 +0300

    finished login page design

---
+++ app/src/main/java/com/sammuiga/emergram/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sammuiga.emergram.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sammuiga.emergram.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
