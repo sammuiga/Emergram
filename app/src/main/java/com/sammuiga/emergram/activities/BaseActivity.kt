9f54652b92ef03d9bea1827608ff39f33f3424a9
sam muiga <sammuiga.m@gmail.com>
2021-09-17 16:00:15 +0300

    Add dialog progress bar

--- app/src/main/java/com/sammuiga/emergram/activities/BaseActivity.kt
+++ app/src/main/java/com/sammuiga/emergram/activities/BaseActivity.kt
@@ -1,12 +1,56 @@
 package com.sammuiga.emergram.activities
 
+import android.app.Dialog
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
 import com.sammuiga.emergram.R
+import kotlinx.android.synthetic.main.dialog_progress.*
 
-class BaseActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_base)
+open class BaseActivity : AppCompatActivity() {
+
+    private lateinit var mProgressDialog: Dialog
+
+        fun showErrorSnackBar(message: String, errorMessage: Boolean) {
+            val snackBar =
+                Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG)
+            val snackBarView = snackBar.view
+
+            if(errorMessage) {
+                snackBarView.setBackgroundColor(
+                    ContextCompat.getColor(
+                        this@BaseActivity,
+                        R.color.colorSnackBarError
+                        )
+                    )
+            }else{
+                snackBarView.setBackgroundColor(
+                    ContextCompat.getColor(
+                        this@BaseActivity,
+                        R.color.colorSnackBarSuccess
+                    )
+                )
+            }
+            snackBar.show()
+        }
+        fun showProgressDialog(text : String) {
+
+            mProgressDialog = Dialog(this)
+            mProgressDialog.setContentView(R.layout.dialog_progress)
+
+            mProgressDialog.tv_progress_text.setText(text)
+
+            mProgressDialog.setCancelable(false)
+            mProgressDialog.setCanceledOnTouchOutside(false)
+
+            mProgressDialog.show()
+        }
+        fun hideProgressDialog() {
+            mProgressDialog.dismiss()
+        }
     }
-}
--- app/src/main/java/com/sammuiga/emergram/activities/RegisterActivity.kt
+++ app/src/main/java/com/sammuiga/emergram/activities/RegisterActivity.kt
@@ -2,14 +2,18 @@ package com.sammuiga.emergram.activities
 
 import android.content.Intent
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.WindowInsets
 import android.view.WindowManager
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.sammuiga.emergram.R
 import kotlinx.android.synthetic.main.activity_register.*
 
-class RegisterActivity : AppCompatActivity() {
+class RegisterActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
@@ -30,6 +34,10 @@ class RegisterActivity : AppCompatActivity() {
             val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
             startActivity(intent)
         }
+
+        btn_register.setOnClickListener{
+            registerUser()
+        }
     }
     private fun setupActionBar() {
         setSupportActionBar(toolbar_register_activity)
@@ -40,5 +48,75 @@ class RegisterActivity : AppCompatActivity() {
             actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_back_24dp)
         }
         toolbar_register_activity.setNavigationOnClickListener {onBackPressed()}
+    }
+    private fun validateRegisterDetails(): Boolean{
+        return when {
+            TextUtils.isEmpty(et_first_name.text.toString().trim { it <= ' '}) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_first_name),true)
+                false
+            }
+            TextUtils.isEmpty(et_last_name.text.toString().trim { it <= ' '}) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_last_name), true)
+                false
+            }
+            TextUtils.isEmpty(et_email.text.toString().trim { it <= ' '}) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_email), true)
+                false
+            }
+            TextUtils.isEmpty(et_password.text.toString().trim { it <= ' '}) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_password), true)
+                false
+            }
+            TextUtils.isEmpty(et_confirm_password.text.toString().trim { it <= ' '}) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_confirm_password), true)
+                false
+            }
+           et_password.text.toString().trim {it <= ' '} != et_confirm_password.text.toString()
+               .trim{it <= ' '} -> {
+                   showErrorSnackBar(resources.getString(R.string.err_msg_password_and_confirm_password_mismatch), true)
+               false
+               }
+            !cb_terms_condition.isChecked -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_agree_terms_and_condition), true)
+                false
+            }
+            else -> {
+                //showErrorSnackBar("Success!!! Thanks For Registering.", false)
+                true
+            }
+
+        }
+
+    }
+
+    private fun registerUser() {
+
+        if(validateRegisterDetails()) {
+
+            showProgressDialog(resources.getString(R.string.please_wait))
+
+            val email:String = et_email.text.toString().trim { it <= ' ' }
+            val password: String = et_password.text.toString().trim { it <= ' ' }
+
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(
+                    OnCompleteListener<AuthResult> { task ->
+
+                        hideProgressDialog()
+
+                        if( task.isSuccessful) {
+
+                            val firebaseUser: FirebaseUser = task.result!!.user!!
+
+                            showErrorSnackBar(
+                                "You are registered successfully. your user id is ${firebaseUser.uid}",
+                                false
+                            )
+                        }else{
+                            showErrorSnackBar(task.exception!!.message.toString(), true)
+                        }
+                    }
+                )
+        }
     }
 }
